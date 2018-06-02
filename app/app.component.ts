9d4001ed2acc3cbccac04a0f9e9bca7927bd6ac8
Samar Shaikh <samar@walkover.in>
2018-06-03 02:11:57 +0530

    samar | property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,20 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      <h1 [innerHTML]="title"></h1>
+      <h1>{{title}}</h1>
+      <img [src]="logo">
+      <input type="text" [value]="name">
+      <div>{{name}}</div>
     </div>
   `
 })
 export class AppComponent {
+  public title: string;
+  public name: string = 'samar';
+  public logo: string = 'img/logo.svg';
 
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
