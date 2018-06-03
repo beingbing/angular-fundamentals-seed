247f23e6f486a690003e9ce53647db85b7001dd2
Samar Shaikh <samar@walkover.in>
2018-06-04 00:10:05 +0530

    samar | route module router and outlet

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router/src/config';
+
 
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'}
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     PassengerDashboardModule
   ],
   bootstrap: [AppComponent],
