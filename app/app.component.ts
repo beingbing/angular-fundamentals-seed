2dc9ffd164cb3ce7f191e9e3ca9e6a3c5e60dbbb
Samar Shaikh <samar@walkover.in>
2018-06-03 11:05:19 +0530

    samar | feature module using @NgModule

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+
+interface Child {
+  name: string,
+  age: number
+}
+
 interface Passenger {
   id: number,
   fullName: string,
   checkedIn: boolean,
-  checkedInDate?: number | null
+  checkedInDate?: number | null,
+  children?: Child[] | null
 }
 
 @Component({
@@ -16,10 +23,12 @@ interface Passenger {
         <li *ngFor="let passenger of passengers;let i = index;">
         <span class="status" [class.checked-in]="passenger.checkedIn"></span>
           {{i+1}}: {{passenger.fullName}}
-          <p>{{passenger | json}}</p>
           <div class="date">
           Check in date: 
           {{passenger.checkedInDate ? (passenger.checkedInDate | date: 'yMMMMd' | uppercase) : 'Not checked In'}}
+          </div>
+          <div class="children">
+            Children: {{passenger.children?.length || 0}}
           </div>
         </li>
       </ul>
@@ -32,27 +41,32 @@ export class AppComponent {
     id: 1,
     fullName: 'Stephen',
     checkedIn: true,
-    checkedInDate: 1490742000000
+    checkedInDate: 1490742000000,
+    children: null
   },{
     id: 2,
     fullName: 'Rose',
     checkedIn: false,
-    checkedInDate: null
+    checkedInDate: null,
+    children: [{name: 'Ted', age: 12},{name: 'Chloe', age: 7}]
   },{
     id: 3,
     fullName: 'James',
     checkedIn: true,
-    checkedInDate: 1491606000000
+    checkedInDate: 1491606000000,
+    children: null
   },{
     id: 4,
     fullName: 'Louise',
     checkedIn: true,
-    checkedInDate: 1488412800000
+    checkedInDate: 1488412800000,
+    children: [{name: 'Jessica', age: 1}]
   },{
     id: 5,
     fullName: 'Tina',
     checkedIn: false,
-    checkedInDate: null
+    checkedInDate: null,
+    children: null
   }];
 
   constructor() {
