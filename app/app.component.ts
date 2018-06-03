337db9198d1139918b96f5d152a60bd2380987f1
Samar Shaikh <samar@walkover.in>
2018-06-03 11:18:36 +0530

    samar | creating a container (smart) component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,74 +1,17 @@
 import { Component } from '@angular/core';
 
-interface Child {
-  name: string,
-  age: number
-}
-
-interface Passenger {
-  id: number,
-  fullName: string,
-  checkedIn: boolean,
-  checkedInDate?: number | null,
-  children?: Child[] | null
-}
-
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers;let i = index;">
-        <span class="status" [class.checked-in]="passenger.checkedIn"></span>
-          {{i+1}}: {{passenger.fullName}}
-          <div class="date">
-          Check in date: 
-          {{passenger.checkedInDate ? (passenger.checkedInDate | date: 'yMMMMd' | uppercase) : 'Not checked In'}}
-          </div>
-          <div class="children">
-            Children: {{passenger.children?.length || 0}}
-          </div>
-        </li>
-      </ul>
+      <passenger-dashboard></passenger-dashboard>
     </div>
   `
 })
 export class AppComponent {
   public name: string = '';
-  public passengers: Passenger[] = [{
-    id: 1,
-    fullName: 'Stephen',
-    checkedIn: true,
-    checkedInDate: 1490742000000,
-    children: null
-  },{
-    id: 2,
-    fullName: 'Rose',
-    checkedIn: false,
-    checkedInDate: null,
-    children: [{name: 'Ted', age: 12},{name: 'Chloe', age: 7}]
-  },{
-    id: 3,
-    fullName: 'James',
-    checkedIn: true,
-    checkedInDate: 1491606000000,
-    children: null
-  },{
-    id: 4,
-    fullName: 'Louise',
-    checkedIn: true,
-    checkedInDate: 1488412800000,
-    children: [{name: 'Jessica', age: 1}]
-  },{
-    id: 5,
-    fullName: 'Tina',
-    checkedIn: false,
-    checkedInDate: null,
-    children: null
-  }];
-
+  
   constructor() {
     //
   }
---
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { Passenger } from '../../models/passenger.interfase';
+
+@Component({
+    selector: 'passenger-dashboard',
+    styleUrls: ['passenger-dashboard.component.scss'],
+    template: `
+        <div>
+        <h3>Airline Passengers</h3>
+        <ul>
+          <li *ngFor="let passenger of passengers;let i = index;">
+          <span class="status" [class.checked-in]="passenger.checkedIn"></span>
+            {{i+1}}: {{passenger.fullName}}
+            <div class="date">
+            Check in date: 
+            {{passenger.checkedInDate ? (passenger.checkedInDate | date: 'yMMMMd' | uppercase) : 'Not checked In'}}
+            </div>
+            <div class="children">
+              Children: {{passenger.children?.length || 0}}
+            </div>
+          </li>
+        </ul>
+        </div>
+    `
+})
+export class PassengerDashboardComponent {
+    public passengers: Passenger[] = [{
+        id: 1,
+        fullName: 'Stephen',
+        checkedIn: true,
+        checkedInDate: 1490742000000,
+        children: null
+    }, {
+        id: 2,
+        fullName: 'Rose',
+        checkedIn: false,
+        checkedInDate: null,
+        children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
+    }, {
+        id: 3,
+        fullName: 'James',
+        checkedIn: true,
+        checkedInDate: 1491606000000,
+        children: null
+    }, {
+        id: 4,
+        fullName: 'Louise',
+        checkedIn: true,
+        checkedInDate: 1488412800000,
+        children: [{ name: 'Jessica', age: 1 }]
+    }, {
+        id: 5,
+        fullName: 'Tina',
+        checkedIn: false,
+        checkedInDate: null,
+        children: null
+    }];
+
+}
