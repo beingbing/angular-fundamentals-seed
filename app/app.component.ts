5d661360c07b81b96a6605a38bd9a5d019026655
Samar Shaikh <samar@walkover.in>
2018-06-03 09:46:16 +0530

    samar | ngClass and class binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,17 +10,17 @@ interface Passenger {
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-    <h3>Airline Passengers</h3>
-    <ul>
-      <ng-template ngFor let-passenger let-i ="index" [ngForOf]="passengers">
-      <li>
-        {{i+1}}: {{passenger.fullName}}
-      </li>
-      </ng-template>
-    </ul>
       <h3>Airline Passengers</h3>
       <ul>
         <li *ngFor="let passenger of passengers;let i = index;">
+        <span class="status" [class.checked-in]="passenger.checkedIn"></span>
+          {{i+1}}: {{passenger.fullName}}
+        </li>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers;let i = index;">
+        <span class="status" [ngClass]="{'checked-in': passenger.checkedIn, 'checked-out': !passenger.checkedIn}"></span>
           {{i+1}}: {{passenger.fullName}}
         </li>
       </ul>
